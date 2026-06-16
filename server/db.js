a72670168cdfe953a8bd683eb4ce335895ed251a
Diobe Belle <philipewang120@gmail.com>
2026-06-16 02:27:50 +0100

    switch from local db to neon

--- server/db.js
+++ server/db.js
@@ -3,18 +3,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-//const db = new pg.Pool({
-  //connectionString: process.env.DATABASE_URL,
- // ssl: { rejectUnauthorized: false },
-//});
-
-
-const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "African Cinema",
-  password: "tutoPostgres@123",
-  port: 5433,
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 db.connect();
